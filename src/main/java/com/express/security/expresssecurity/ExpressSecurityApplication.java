dc902e0fe366951a044d8739bbba1de944d47413
ketsar28 <muhammadketsar1@gmail.com>
2023-11-15 14:16:11 +0700

    [INITIAL] starter project

---
+++ src/main/java/com/express/security/expresssecurity/ExpressSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.express.security.expresssecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpressSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpressSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/express/security/expresssecurity/ExpressSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.express.security.expresssecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpressSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
